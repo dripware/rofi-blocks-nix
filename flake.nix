5d4be2b7eac715b3cb0a27199b3a3c283e9b000d
dripware <itsdripware@gmail.com>
2022-06-29 23:12:11 +0430

    add system to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "a nix flake for rofi-blocks";
   outputs = { self, nixpkgs }: {
+    system = "x86_64-linux";
     overlay = (self: super:{
       rofi-blocks = import ./build.nix nixpkgs;
     });
